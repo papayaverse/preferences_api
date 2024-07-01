4df7123a238c09881d86661577551650d1267c15
Ram Kripa <ramkripa@rams-air.attlocal.net>
2024-07-01 14:13:54 -0500

    changed api code to require auth for preferences

--- fast_api_app.py
+++ fast_api_app.py
@@ -20,18 +20,17 @@ class User(BaseModel):
     email: str
     password: str
 
-app.users = {'ram@papayaverse.com' : User(firstname='Ram', lastname='Test', email='ram@papayaverse.com', password='password')}
-app.user_consent_preferences = {'ram@papayaverse.com' : {'default': ConsentPreferences(marketing=False, performance=True, sell_data=False)}}
-
+app.users = {}
+app.user_consent_preferences = {}
 app.sessions = {}
 
 @app.get('/hello')
 def hello():
-    return {"hello" : "world!"}
+    return {"hello": "world!"}
 
 @app.get('/hello/{personName}')
 def hello_person(personName):
-    return {"hello" : personName}
+    return {"hello": personName}
 
 # Create Account Endpoint
 @app.post("/createAccount")
@@ -47,13 +46,13 @@ def sign_up(user: User):
 
 # Helper function to create a session in which we can store state about the user
 def create_session(user_email: str):
-    session_id = str(len(app.sessions))# + random.randint(0, 1000))
+    session_id = str(len(app.sessions))
     app.sessions[session_id] = {"user_email": user_email}
     return session_id
 
 # Helper function to authenticate the User
 def authenticate_user(credentials: HTTPBasicCredentials = Depends(security)):
-    user = app.users.get(credentials.username) # we get the user based on their email id
+    user = app.users.get(credentials.username)
     if user is None or user.password != credentials.password:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -68,7 +67,7 @@ def login(user: User = Depends(authenticate_user)):
     session_id = create_session(user.email)
     return {"message": f"User {user.email} Logged in successfully", "session_id": session_id}
 
-# Get auhtenticated user from session_id or credentials
+# Custom dependency to handle both session ID and Authorization header
 def get_authenticated_user(request: Request, credentials: HTTPBasicCredentials = Depends(security)):
     # Try to get user from session ID in cookies
     session_id = request.cookies.get("session_id")
@@ -96,24 +95,11 @@ def get_authenticated_user(request: Request, credentials: HTTPBasicCredentials =
         headers={"WWW-Authenticate": "Basic"},
     )
 
-# Helper to get user from session_id
-def get_authenticated_user_from_session_id(request: Request):
-    session_id = request.cookies.get("session_id")
-    if session_id is None or session_id not in app.sessions:
-        raise HTTPException(
-            status_code=401,
-            detail="Invalid session ID",
-        )
-    # Get the user from the session
-    user = app.users[app.sessions[session_id]["user_email"]]
-    return user
-
 # Endpoint to get site preferences
 @app.get("/preferences/{site}")
-def prefs(site: str, user: User = Depends(get_authenticated_user)):
-    if user is None:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authenticated")
-    elif user.email not in app.user_consent_preferences:
+def prefs(site: str, credentials: HTTPBasicCredentials = Depends(security)):
+    user = authenticate_user(credentials)
+    if user.email not in app.user_consent_preferences:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User does not have preferences yet")
     else:
         if site not in app.user_consent_preferences[user.email]:
@@ -122,13 +108,12 @@ def prefs(site: str, user: User = Depends(get_authenticated_user)):
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User {user.email} does not have preferences for this site or default") 
         else:
             return app.user_consent_preferences[user.email][site]
+
 # Endpoint to set site preferences
 @app.post("/preferences/{site}")
-def prefs(site: str, consent: ConsentPreferences, user: User = Depends(get_authenticated_user)):
-    if user is None:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authenticated")
-    else :
-        if user.email not in app.user_consent_preferences:
-            app.user_consent_preferences[user.email] = {}
-        app.user_consent_preferences[user.email][site] = consent
-        return {"message": f"Set preferences successfully for user {user.email} for site {site}"}
+def prefs(site: str, consent: ConsentPreferences, credentials: HTTPBasicCredentials = Depends(security)):
+    user = authenticate_user(credentials)
+    if user.email not in app.user_consent_preferences:
+        app.user_consent_preferences[user.email] = {}
+    app.user_consent_preferences[user.email][site] = consent
+    return {"message": f"Set preferences successfully for user {user.email} for site {site}"}
